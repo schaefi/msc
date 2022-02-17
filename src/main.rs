102e22f2bd3b4a94d6ec62c7798e32beb534cf17
Marcus Schäfer <marcus.schaefer@gmail.com>
2022-02-17 22:35:18 +0100

    Experimenting, learning, coffee :)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use text_io::read;
 use http_auth_basic::Credentials;
-// use std::collections::HashMap;
 use error_chain::error_chain;
 use minidom::Element;
+use rpassword::read_password;
+use std::io::Write;
 
 mod cli;
 
@@ -14,7 +14,6 @@ error_chain! {
 }
 
 #[tokio::main]
-//async fn main() -> Result<(), Box<dyn std::error::Error>> {
 async fn main() -> Result<()> {
     let mut args = cli::parse_args();
 
@@ -22,19 +21,43 @@ async fn main() -> Result<()> {
     // provided insecurely on the command line
     match args.password {
         None => {
-            println!("Enter OBS password for user ({}): ", args.user);
-            args.password = Some(read!("{}\n"));
+            print!("Enter OBS password for user ({}): ", args.user);
+            std::io::stdout().flush().unwrap();
+            args.password = Some(read_password().unwrap());
         },
         _ => (),
     }
 
     println!("Hi {:?}", args.password.as_deref());
 
-    let x = get_request().await?;
-    //println!("{:?}", x);
+    // https://api.opensuse.org/source/Virtualization:Appliances:Images:Testing_x86:leap/test-image-luks?expand=1
+
+    let url;
+    match &args.command {
+        cli::Commands::Checkout { package } => {
+            url = build_source_endpoint_url(
+                &args.api_server, &args.project, Some(package), true
+            );
+        },
+        // TODO: handle other sub commands
+        _ => { url = String::new() },
+    }
+
+    // FIXME: url should not be empty
+    assert_eq!(url.is_empty(), false);
+
+    println!("{}", url);
+
+    let response_text = send_get_request(
+        &args.user, args.password.as_ref().unwrap(), &url
+    ).await?;
+
+    // FIXME: handling of auth errors 401, and other that causes html response
+    println!("{}", response_text);
+
     let mut xml = String::new();
     xml.push_str("<response xmlns=\"directory\">");
-    xml.push_str(&x);
+    xml.push_str(&response_text);
     xml.push_str("</response>");
      
     let root: Element = xml.parse().unwrap();
@@ -43,29 +66,42 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-//async fn get_request() -> Result<String, Box<dyn std::error::Error>> {
-async fn get_request() -> Result<String> {
-    let credentials = Credentials::new("foo", "bar");
+fn build_source_endpoint_url(
+    server: &String, project: &String, package: Option<&String>, expand: bool
+) -> String {
+    let mut url;
+    match package {
+        None => {
+            url = format!("{}/source/{}", server, project);
+        }
+        _ => {
+            url = format!("{}/source/{}/{}", server, project, package.unwrap());
+        }
+    }
+    if expand {
+        url = format!("{}?expand=1", url);
+    }
+    url
+}
+
+async fn send_get_request(
+    user: &String, password: &String, url: &String
+) -> Result<String> {
+    /*!
+    Send GET request to the API server and return
+    an XML Element root object
+    !*/
+    let credentials = Credentials::new(user, password);
 
     let client = reqwest::Client::builder()
         .build()?;
 
-    // Perform the actual execution of the network request
     let res = client
-        .get("https://api.opensuse.org/source/Virtualization:Appliances:Images:Testing_x86:leap/test-image-luks?expand=1")
+        .get(url)
         .header("Authorization", credentials.as_http_header())
         .send()
         .await?;
 
-    // Parse the response body as Json in this case
-    let ip = res
-        //.json::<HashMap<String, String>>()
-        .text()
-        .await?;
-
-    //println!("{:?}", ip);
-
-    //Ok(())
-    //std::result::Result::Ok(ip)
-    Ok(ip)
+    let response_content = res.text().await?;
+    Ok(response_content)
 }
