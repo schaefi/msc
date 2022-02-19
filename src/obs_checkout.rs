ba9d4ec0881ec5d25e2809335745cb658b62e8fc
Marcus Schäfer <marcus.schaefer@gmail.com>
2022-02-19 23:12:40 +0100

    Added fetcher

--- src/obs_checkout.rs
+++ src/obs_checkout.rs
@@ -1,5 +1,7 @@
 use crate::obs_net::build_source_endpoint_url;
 use crate::obs_net::send_request;
+use crate::obs_net::fetch_file;
+use crate::obs_net::get_text;
 use crate::obs_net::to_xml;
 use crate::obs_connect::Obs;
 
@@ -17,10 +19,16 @@ pub async fn checkout(
     let response = send_request(
         &connect.user, &connect.password, &url
     ).await?;
-    let response_xml_root = to_xml(&response);
-
-    // TODO
+    let response_xml_root = to_xml(&get_text(response).await?);
     println!("{:?}", response_xml_root);
     println!("{}", outdir);
+
+    // TODO
+    let fetch_url = build_source_endpoint_url(
+        &connect.api_server, &connect.project, Some(&String::from("test-image-luks/_service:obs_scm:appliance.kiwi")), true
+    );
+    let response = send_request(&connect.user, &connect.password, &fetch_url).await?;
+    fetch_file(response, &fetch_url, &format!("/home/ms/msc_fetched")).await?;
+
     Ok(())
 }
--- src/obs_net.rs
+++ src/obs_net.rs
@@ -1,6 +1,11 @@
 use http_auth_basic::Credentials;
 use minidom::Element;
 use std::io::{Error, ErrorKind};
+use std::cmp::min;
+use std::fs::File;
+use std::io::Write;
+use indicatif::{ProgressBar, ProgressStyle};
+use futures_util::StreamExt;
 
 pub fn build_source_endpoint_url(
     server: &String, project: &String, package: Option<&String>, expand: bool
@@ -37,30 +42,81 @@ pub fn to_xml(data: &String) -> Element {
     root
 }
 
-pub async fn send_request(
-    user: &String, password: &String, url: &String
+pub async fn get_text(
+    response: reqwest::Response
 ) -> Result<String, Box<dyn std::error::Error>> {
     /*!
-    Send GET request to the API server and return
-    an XML Element root object
+    Return text from response
+    !*/
+    let response_content = response.text().await?;
+    Ok(response_content)
+}
+
+pub async fn fetch_file(
+    response: reqwest::Response, url: &String, filepath: &String
+) -> Result<(), Box<dyn std::error::Error>> {
+    let total_size = response
+        .content_length()
+        .ok_or(format!("Failed to get content length from '{}'", &url))?;
+    let progress = ProgressBar::new(total_size);
+
+    progress.set_style(ProgressStyle::default_bar()
+        .template(
+            &format!(
+                "{}\n{} [{}] [{}] {}/{} ({}, {})",
+                "{msg}",
+                "{spinner:.green}",
+                "{elapsed_precise}",
+                "{wide_bar:.cyan/blue}",
+                "{bytes}",
+                "{total_bytes}",
+                "{bytes_per_sec}",
+                "{eta}"
+            )
+        )
+        .progress_chars("#>-"));
+    progress.set_message(&format!("Downloading {}", url));
+
+    let mut file = File::create(filepath)?;
+    let mut downloaded: u64 = 0;
+    let mut stream = response.bytes_stream();
+
+    while let Some(item) = stream.next().await {
+        let chunk = item?;
+        file.write_all(&chunk)?;
+        let new = min(downloaded + (chunk.len() as u64), total_size);
+        downloaded = new;
+        progress.set_position(new);
+    }
+    progress.finish_with_message(
+        &format!("Downloaded {} to {}", url, filepath)
+    );
+    return Ok(());
+}
+
+pub async fn send_request(
+    user: &String, password: &String, url: &String
+) -> Result<reqwest::Response, Box<dyn std::error::Error>> {
+    /*!
+    Send GET request to the API server and return response object
     !*/
     let credentials = Credentials::new(user, password);
 
     let client = reqwest::Client::builder()
         .build()?;
 
-    let res = client
+    let response = client
         .get(url)
         .header("Authorization", credentials.as_http_header())
         .send()
         .await?;
 
-    match res.status() {
+    match response.status() {
         reqwest::StatusCode::BAD_REQUEST => {
             let request_error = format!(
                 "content-length:{:?} server:{:?}",
-                res.headers().get(reqwest::header::CONTENT_LENGTH),
-                res.headers().get(reqwest::header::SERVER),
+                response.headers().get(reqwest::header::CONTENT_LENGTH),
+                response.headers().get(reqwest::header::SERVER),
             );
             return Err(
                 Box::new(Error::new(ErrorKind::InvalidData, request_error))
@@ -75,6 +131,5 @@ pub async fn send_request(
             }
         },
     }
-    let response_content = res.text().await?;
-    Ok(response_content)
+    Ok(response)
 }
